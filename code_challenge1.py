e356be9cbaa76e184c3fede788db2442ba215535
RenzArandela <renzarandela112@gmail.com>
2025-08-17 15:26:04 +0800

    code_challenge1.py

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name = input("What is your name?: ")
+print("        *\n       * *\n      *   *\n     *     *\n    *       *\n   *         *\n  *   Hi " + name + "   *\n   *         *\n    *       *\n     *     *\n      *   *\n       * *\n        *")
