aa00a1ebe40f4df2ac88a1d686d52df43f68f4ff
RenzArandela <renzarandela112@gmail.com>
2025-08-30 17:12:03 +0800

    activity10.py

---
+++ activity10.py
@@ -0,0 +1,10 @@
+name = 'haha'
+pword = 'hihi'
+
+Username = input("Enter your name here -->  ")
+password = input("enter your password here --> ")
+
+if name == Username and pword == password:
+    print("You have granted an access")
+else:
+    print("Your access has been denied")
