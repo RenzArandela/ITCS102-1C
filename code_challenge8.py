2a97d4f206254cc496b08d3d138a59821a479e4e
RenzArandela <renzarandela112@gmail.com>
2025-09-17 18:39:59 +0800

    Create code_challenge8.py

---
+++ code_challenge8.py
@@ -0,0 +1,15 @@
+import time
+
+print("⏳ COUNTDOWN TIMER SIMULATOR")
+
+start = eval(input("Enter the starting number for countdown: "))
+
+print()
+print("Countdown started:")
+
+for i in range(start, 0, -1):
+    print(i)
+    time.sleep(0.5)
+
+time.sleep(0.5)
+print("Liftoff!")
