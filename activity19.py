545278b17116c78801686635bde567466aed4ddf
RenzArandela <renzarandela112@gmail.com>
2025-10-05 17:41:09 +0800

    Create activity19.py

---
+++ activity19.py
@@ -0,0 +1,18 @@
+print("\t\t\t\t *", end=' ')
+for b in range(1,10,1):
+    for i in range(10,b,-1):
+        print(' ',end=' ')
+    for c in range(1,b,1):
+        print('*',end=' ')
+    for o in range(1,b,1):
+        print('*',end=' ')
+    print()
+for m in range(10,1,-1):
+    for i in range(10,b,-1):
+        print(' ',end=' ')
+    for c in range(1,b,1):
+        print('*',end=' ')
+    for o in range(1,b,1):
+        print('*',end=' ')
+    print()
+print("\t\t\t\t *", end=' ')
