0e2c402607f5b672c290f53ef8e98fa17b5e316a
RenzArandela <renzarandela112@gmail.com>
2025-10-15 14:17:05 +0800

    Create activity20.py

---
+++ activity20.py
@@ -0,0 +1,11 @@
+isDirty = True 
+
+while isDirty == True:
+     wash_again = input ("continue washing the clothes yes / no --> ").lower()
+
+     if wash_again == "yes":
+          print("washing the clothes again...")
+          continue
+     else:
+          print("washing stops now...")
+          break
