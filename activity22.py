4e78ed6ded3ef7175c608362d2c2084ad4db0721
RenzArandela <renzarandela112@gmail.com>
2025-10-27 13:37:20 +0800

    Create activity22.py

---
+++ activity22.py
@@ -0,0 +1,15 @@
+months = ("january","february","march","april","may","june")
+
+print(months)
+
+months.reverse()
+print(months)
+
+fullname = 'Renz Darrel Arandela'
+
+newlist = list(fullname)
+new_list.reverse()
+print(new_list)
+
+new_list.sort()
+print(newlist)
