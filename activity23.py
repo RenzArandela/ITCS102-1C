f8fc09a7d686c99674b7954ddb3d5985f42004da
RenzArandela <renzarandela112@gmail.com>
2025-11-12 14:06:46 +0800

    Create activity23.py

---
+++ activity23.py
@@ -0,0 +1,14 @@
+from activity23_1 import Greetwithname, Greetperson, FunctionwithReturn, Factorial
+
+def Myfirstfunction():
+    print("hoy")
+    print("hi")
+    print("ha")
+
+Myfirstfunction()
+
+Greetwithname("Arandela")
+Greetperson('Renz', 'Philippines', '18')
+
+print(f"I want to get the summation of {FunctionwithReturn(8)}")
+print(f"The factorial of 5 is {Factorial(5)}")
