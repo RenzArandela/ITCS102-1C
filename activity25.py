eb762a791a5ab0df97efc8fd6f0d4c7923ee9cf0
RenzArandela <renzarandela112@gmail.com>
2025-11-17 14:29:53 +0800

    Create activity25.py

---
+++ activity25.py
@@ -0,0 +1,11 @@
+programming_language = ['java', 'C#','C++','C','Perl','Python','JavaScript','Rust',"Golang"]
+
+print(programming_language[-1])
+
+empty_dictionary = {}
+
+pl_dictionary = { 'Medyo Mahirap':'java', 'Tama lang': 'C#', 'Mahirap talaga': 'C++', 'luma na':  'C', 
+                 'pang matanda': 'Perl', 'ge': 'Python', "hi": 'JavaScript' }
+
+
+print(pl_dictionary("Tama lang"))
