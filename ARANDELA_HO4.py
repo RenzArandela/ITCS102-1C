cf471f25755cce6d24d01dba38cd1561e553ae92
RenzArandela <renzarandela112@gmail.com>
2026-03-16 15:47:48 +0800

    Update ARANDELA_HO4.py

--- ARANDELA_HO4.py
+++ ARANDELA_HO4.py
@@ -1,57 +1,29 @@
-import tkinter as  tk
+import tkinter as tk
 
+window = tk.Tk()
+window.title("Profile Builder")
+window.geometry("600x300")
+window.configure(bg="lightgray")
 
-window = tk.Tk
+tk.Label(window, text="Profile Builder", bg="lightgray").grid(row=0,column=1)
 
-window.title("Profile Builder ")
-window.geometry("600x600")
-window.resizable(False, True)
+tk.Entry(window).grid(row=1,column=0)
+tk.Entry(window).grid(row=1,column=1)
+tk.Entry(window).grid(row=1,column=2)
 
-window.configure(bg="lightblue")
+tk.Label(window,text="First").grid(row=2,column=0)
+tk.Label(window,text="Middle").grid(row=2,column=1)
+tk.Label(window,text="Last").grid(row=2,column=2)
 
-frame =tk.frame(window, bg = "light blue"  )
+tk.Entry(window).grid(row=3,column=0)
+tk.Label(window,text="Birth Year").grid(row=4,column=0)
 
-label = tk.Label(window,text="profile Builder",font ="Times New Roman",fg = "blue",bg = "lightblue")
-label.pack()
+tk.Label(window,text="Age").grid(row=3,column=1)
 
-entry = tk.Entry
-entry.pack()
+tk.Label(window,text="Gender").grid(row=5,column=0)
+tk.Radiobutton(window,text="Male").grid(row=5,column=1)
+tk.Radiobutton(window,text="Female").grid(row=5,column=2)
 
+tk.Button(window,text="Submit").grid(row=6,column=1)
 
-label = tk.Label(window,text="First name",font ="Times New Roman",fg = "blue",bg = "lightblue")
-label.pack()
-
-entry = tk.Entry
-entry.pack()
-
-
-label = tk.Label(window,text="Last Name",font ="Times New Roman",fg = "blue",bg = "lightblue")
-label.pack()
-
-entry = tk.Entry
-entry.pack()
-
-
-label = tk.Label(window,text="Age",font ="Times New Roman",fg = "blue",bg = "lightblue")
-label.pack()
-
-entry = tk.Entry
-entry.pack()
-
-
-
-
-entry = tk.Entry
-entry.pack()
-
-
-def compute():
-    
-    
-
-
-
-
-
-
-
+window.mainloop()
