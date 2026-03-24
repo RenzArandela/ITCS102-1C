d6d96699cd4be86f4747a1bdf21b50c7be4a5bbe
RenzArandela <renzarandela112@gmail.com>
2026-03-25 00:10:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,79 +1,36 @@
-import os
+from tkinter import *
 
-os.system('cls')
-print('DLL Student Information system')
-print('===================================')
+app = Tk()
+app.title("Simple Account System")
+app.geometry("300x400")
 
-stud_record = {}
+stored_username = ""
+stored_password = ""
 
-while True:
-    print("Select from the following options")
-    print('A - Adding Student Record')
-    print('B - Printing Student Record')
-    print('C - Search Student Record')
-    print('D - Delete Student Record')
-    print('E - Retake')
-    print('F - Export Data')
-    print('G - Exit System')
 
-    choice = input("Input your choice --> ").lower().strip()
+Label(app, text="REGISTER").pack()
 
-    if choice == 'a':
-        print("ADD STUDENT RECORD")
-        student_id = input("Input Student ID number: ")
-        first_name = input("Input Student First Name: ").upper()
-        last_name = input("Input Student Last Name: ").upper()
-        course = input("Input Student Course: ").upper()
-        section = input("Input Student Section: ").upper()
-        email = input("Input Student Email: ").upper()
-        stud_record[student_id] = [first_name, last_name, course, section, email]
-        print("Data saved Successfully")
-        os.system('cls')
+Label(app, text="Username").pack()
+reg_user = Entry(app)
+reg_user.pack()
 
-        continue
+Label(app, text="Password").pack()
+reg_pass = Entry(app)
+reg_pass.pack()
 
-    elif choice == "b":
-        os.system("cls")
-        print("PRINTING STUDENT RECORD")
-        print("====================================")
+Button(app, text="Register").pack()
 
-        for id,info in stud_record.items():
-            print(f"STUDENT ID {id} - RECORD - {info}")
 
-            continue
+Label(app, text="LOGIN").pack()
 
-    elif choice == 'c':
-        os.system("cls")
-        print("SEARCH STUDENT RECORD")
+Label(app, text="Username").pack()
+log_user = Entry(app)
+log_user.pack()
 
-        search_id = input("INPUT STUDENT ID ------- > ").lower()
+Label(app, text="Password").pack()
+log_pass = Entry(app)
+log_pass.pack()
 
-        for each_student in stud_record.keys():
-            if search_id in stud_record.keys():
-                print(f"RECORD FOUND for {search_id}")
-                print("====================")
-            else:
-                print("RECORD NOT FOUND")
-            for id, in stud_record[search_id]: 
-                print(f" --{id}")
-                print("============")
-            else:
-                print("no record found")
-            break
-        continue
+Button(app, text="Log In").pack()
 
-    elif choice == 'd':
-        os.system("cls")
-    print("DELETE STUDENT RECORD")
-
-    search_id = input("INPUT STUDENT ID --> ")
-
-    if search_id in stud_record:
-        stud_record.pop(search_id)
-        print("RECORD DELETED SUCCESSFULLY")
-    else:
-        print("RECORD NOT FOUND")
-
-    continue
-
-    elif choice == "e"
+app.mainloop()  
